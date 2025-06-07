6fcaa8ff2aa3ad0b876dbd4f1474d34228527063
César Bravo <cbravo@bcyt.cl>
2025-06-07 19:24:51 -0400

    bff

--- src/main/java/com/example/bff/controller/TicketController.java
+++ src/main/java/com/example/bff/controller/TicketController.java
@@ -1,12 +1,13 @@
 package com.example.bff.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 import reactor.core.publisher.Mono;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ticket")
@@ -18,47 +19,11 @@ public class TicketController {
         this.webClient = webClient;
     }
 
-    @GetMapping
-    public Mono<List<JsonNode>> getAllTickets() {
+    @GetMapping("/gen")
+    public Mono<JsonNode> generateTicket() {
         return webClient.get()
-                .uri("/ticket")
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<JsonNode>>() {});
-    }
-
-    @GetMapping("/{id}")
-    public Mono<JsonNode> getTicket(@PathVariable String id) {
-        return webClient.get()
-                .uri("/ticket/{id}", id)
+                .uri("/ticket/gen")
                 .retrieve()
                 .bodyToMono(JsonNode.class);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Mono<JsonNode> createTicket(@RequestBody JsonNode ticket) {
-        return webClient.post()
-                .uri("/ticket")
-                .bodyValue(ticket)
-                .retrieve()
-                .bodyToMono(JsonNode.class);
-    }
-
-    @PutMapping("/{id}")
-    public Mono<JsonNode> updateTicket(@PathVariable String id, @RequestBody JsonNode ticket) {
-        return webClient.put()
-                .uri("/ticket/{id}", id)
-                .bodyValue(ticket)
-                .retrieve()
-                .bodyToMono(JsonNode.class);
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Mono<Void> deleteTicket(@PathVariable String id) {
-        return webClient.delete()
-                .uri("/ticket/{id}", id)
-                .retrieve()
-                .bodyToMono(Void.class);
     }
 } 
